547e079c1d285cbd01218a9b9acb8f02147c333c
h600853 <89258011+h600853@users.noreply.github.com>
2023-03-28 13:26:11 +0200

    Add project files.

---
+++ Oblig3/Editor.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Oblig3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Oblig3
+{
+    /// <summary>
+    /// Interaction logic for Editor.xaml
+    /// </summary>
+    public partial class Editor : Window
+    {
+        public Dat154Context Context { get; set; }
+        
+        public Editor(Dat154Context context)
+        {
+            InitializeComponent();
+            Context = context;
+          
+            Cb.ItemsSource = Context.Courses.Select(c => c.Coursecode).OrderBy(c => c).ToList();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            var selecteditem = Cb.SelectedItem;
+            if (selecteditem != null)
+            {
+
+                Student s = new Student()
+                {
+                    Studentname = sname.Text,
+                    Id = int.Parse(sid.Text),
+                    Studentage = int.Parse(sage.Text)
+    
+
+            };
+                Grade g = new Grade()
+                {
+                    Studentid = s.Id,
+                    Coursecode = selecteditem.ToString(),
+                    Grade1 = sgrade.Text   
+                
+
+                };
+
+            Context.Students.Add(s);    
+            Context.Grades.Add(g);  
+            Context.SaveChanges();  
+                sid.Text = sname.Text = sage.Text = sgrade.Text = "";
+            }
+        }
+
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(sid.Text);   
+            Student s = Context.Students.Where(s => s.Id == id).First();    
+            Grade g = Context.Grades.Where(g => g.Studentid == id).First();
+            if (s != null)
+            {
+            Context.Grades.Remove(g);
+            Context.Students.Remove(s); 
+            Context.SaveChanges();
+                sid.Text = sname.Text = sage.Text = sgrade.Text = "";
+            }
+        }
+    }
+}
---
+++ Oblig3/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Oblig3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oblig3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dat154Context dx = new();
+
+        private readonly LocalView<Student> Students;
+        private readonly LocalView<Course> Courses;
+        private readonly LocalView<Grade> Grades;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = dx.Students.Local;
+            dx.Students.Load();
+            studentList.DataContext = Students.OrderBy(s => s.Studentname);
+
+            cbox.SelectionChanged += cbox_SelectionChanged;
+            Courses = dx.Courses.Local;
+            dx.Courses.Load();
+
+            cbox.DataContext = Courses.Select(c => c.Coursecode).OrderBy(c => c);
+            
+            Grades = dx.Grades.Local;
+            dx.Grades.Load();
+
+            combobox.ItemsSource = Grades.Select(g => g.Grade1).OrderBy(g => g).Distinct();
+        }
+
+
+        private void sokeknapp_Click(object sender, RoutedEventArgs e)
+        {
+            studentList.DataContext = Students.Where(s => s.Studentname.Contains(tbox.Text))
+                .OrderBy(s => s.Studentname);
+        }
+
+        private void cbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedvalue = cbox.SelectedItem;
+            if (selectedvalue != null)
+            {
+
+
+            var studentsInCourse = from stud in Students
+                                   join grade in Grades
+                                   on stud.Id equals grade.Studentid
+                                   join course in Courses
+                                   on grade.Coursecode equals course.Coursecode
+                                   where course.Coursecode.Equals(selectedvalue)
+                                   select new { stud.Studentname, grade.Grade1 };
+            studentList.DataContext = studentsInCourse.OrderBy(g => g.Grade1);
+            }
+
+       
+        }
+
+        private void combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedvalue = combobox.SelectedItem;
+            if (selectedvalue != null)
+            {
+                string selectedGrade = selectedvalue.ToString();
+
+                var studentsInCourse = from stud in Students
+                                       join grade in Grades
+                                       on stud.Id equals grade.Studentid
+                                       join course in Courses
+                                       on grade.Coursecode equals course.Coursecode
+                                       where string.Compare(grade.Grade1, selectedGrade) <= 0
+                                       select new { stud.Studentname, grade.Grade1 , course.Coursecode };
+                studentList.DataContext = studentsInCourse.OrderBy(g => g.Grade1);
+            }
+        }
+
+        private void GetF_Click(object sender, RoutedEventArgs e)
+        {
+
+            var studentsInCourse = from stud in Students
+                                   join grade in Grades
+                                   on stud.Id equals grade.Studentid
+                                   join course in Courses
+                                   on grade.Coursecode equals course.Coursecode
+                                   where grade.Grade1.Equals("F")
+                                   select new { stud.Studentname, grade.Grade1, course.Coursecode };
+            studentList.DataContext = studentsInCourse.OrderBy(s => s.Studentname);
+        }
+
+        private void Editor_Click(object sender, RoutedEventArgs e)
+        {
+            Editor ed = new Editor(dx)
+            {
+                Context = dx
+             
+            };
+            ed.Show();
+        }
+    }
+}
